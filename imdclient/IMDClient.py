93ae9df789579d7398667323411c51d9f2e88c8b
Amruthesh Thirumalaiswamy <amruthesht@users.noreply.github.com>
2024-09-18 01:21:59 -0700

    IMDv3 new order

--- imdclient/IMDClient.py
+++ imdclient/IMDClient.py
@@ -563,6 +563,12 @@ class IMDProducerV3(BaseIMDProducer):
             logger.debug(
                 f"IMDProducer: Time: {self._imdf.time}, dt: {self._imdf.dt}, step: {self._imdf.step}"
             )
+        if self._imdsinfo.energies:
+            self._expect_header(IMDHeaderType.IMD_ENERGIES, expected_value=1)
+            self._read(self._energies)
+            self._imdf.energies.update(
+                parse_energy_bytes(self._energies, self._imdsinfo.endianness)
+            )
         if self._imdsinfo.box:
             self._expect_header(IMDHeaderType.IMD_BOX, expected_value=1)
             self._read(self._box)
@@ -601,13 +607,6 @@ class IMDProducerV3(BaseIMDProducer):
                 np.frombuffer(
                     self._forces, dtype=f"{self._imdsinfo.endianness}f"
                 ).reshape((self._n_atoms, 3)),
-            )
-
-        if self._imdsinfo.energies:
-            self._expect_header(IMDHeaderType.IMD_ENERGIES, expected_value=1)
-            self._read(self._energies)
-            self._imdf.energies.update(
-                parse_energy_bytes(self._energies, self._imdsinfo.endianness)
             )
 
     def __del__(self):
