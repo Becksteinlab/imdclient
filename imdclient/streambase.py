4e9d607094a7acef6b34388f6b1a8c961078ed83
ljwoods2 <ljwoods2@asu.edu>
2024-10-13 21:43:10 -0700

    typos

--- imdclient/streambase.py
+++ imdclient/streambase.py
@@ -15,6 +15,7 @@ class StreamReaderBase(ReaderBase):
         )
         self._init_scope = True
         self._reopen_called = False
+        self._first_ts = None
 
     def _read_next_timestep(self):
         # No rewinding- to both load the first frame after  __init__
