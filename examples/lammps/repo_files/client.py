f7441a0ab2ddc80c6b17be2333353ee155c5669a
Amruthesh Thirumalaiswamy <amruthesht@users.noreply.github.com>
2025-01-27 15:38:45 -0700

    chore: added client.py files and cleaned uo output data from examples

---
+++ examples/lammps/repo_files/client.py
@@ -0,0 +1,20 @@
+from imdclient.IMD import IMDReader
+import MDAnalysis as mda
+import logging
+from imdclient.tests.datafiles import LAMMPS_TOPOL
+
+logger = logging.getLogger("imdclient.IMDClient")
+file_handler = logging.FileHandler("imdreader.log")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+i = 0
+u = mda.Universe(LAMMPS_TOPOL, "imd://localhost:8888")
+for ts in u.trajectory:
+    i += 1
+
+logger.info(f"Parsed {i} frames")
